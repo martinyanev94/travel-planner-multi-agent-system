94eae7130a63c2e9127ab46d60d1bf0abccf9b2f
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-11-22 03:36:27 -0500

    Add Lesson_13_Text_Normalization_Techniques/part_3_tokenization_example.py

---
+++ Lesson_13_Text_Normalization_Techniques/part_3_tokenization_example.py
@@ -0,0 +1,9 @@
+import nltk
+from nltk.tokenize import word_tokenize
+
+# Sample text
+text = "Hello, World! Welcome to NLP. Let's tokenize this text."
+
+# Tokenizing the text
+tokens = word_tokenize(text)
+print(tokens)
