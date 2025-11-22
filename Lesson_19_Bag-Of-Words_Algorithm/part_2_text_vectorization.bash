31e52a5684ef25340fefc6aa7b5309f5a5a11f9e
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-11-22 03:37:11 -0500

    Add Lesson_19_Bag-Of-Words_Algorithm/part_2_text_vectorization.bash

---
+++ Lesson_19_Bag-Of-Words_Algorithm/part_2_text_vectorization.bash
@@ -0,0 +1,9 @@
+pip install scikit-learn
+from sklearn.feature_extraction.text import CountVectorizer
+
+# Sample Text
+documents = [
+    "The cat sat on the mat.",
+    "The dog sat on the log.",
+    "Cats and dogs are great pets."
+]
